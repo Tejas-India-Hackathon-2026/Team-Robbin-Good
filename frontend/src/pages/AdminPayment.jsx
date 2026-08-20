9dcb26fc6776a6fc08ccbfb892a5a1835d7a941c
Pradeep Kumar <pradeepkumar620523@gmail.com>
2026-08-20 18:22:48 +0530

    feat: enable recipient autofill in payment form

--- frontend/src/pages/AdminPayment.jsx
+++ frontend/src/pages/AdminPayment.jsx
@@ -81,6 +81,7 @@ export default function AdminPayment({ onPaymentRecorded }) {
                         id="payment-recipient"
                         name="recipient"
                         type="text"
+                        autoComplete="name"
                         value={form.recipient}
                         onChange={handleChange}
                         className={inputClass}
